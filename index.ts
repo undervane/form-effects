842469e693fb9c4459b557ed88a862e188e33228
Undervane <sergio@mipigu.com>
2019-05-11 10:41:51 +0200

    [dev] Migrated to standard Angular lib convention

--- index.ts
+++
@@ -1,8 +0,0 @@
-
-export * from './src/form-effects.module';
-export * from './src/directives/form-effect-name.directive';
-export * from './src/directives/form-effects.directive';
-export * from './src/model/form-effects.type';
-export * from './src/interfaces/effect-dispatch.interface';
-export * from './src/interfaces/effect-result.interface';
-export * from './src/interfaces/effect.interface';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormEffectsModule } from 'form-effects';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormEffectsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
